a922280d64d0291f52be66d7de471c0d8a8bdbd2
Raisess <danilosantana456789@gmail.com>
2021-12-05 11:31:36 -0300

    cpu: implement clocks

--- src/Computer/Cpu.cpp
+++ src/Computer/Cpu.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
 #include "Cpu.h"
-#include "Bus.h"
 
-Computer::Cpu::Cpu() {}
+Computer::Cpu::Cpu(void) {
+  this->instructions = {};
+}
+Computer::Cpu::~Cpu(void) {}
+
+void Computer::Cpu::set_flag(FLAGS flag, bool value) {
+  value ? this->flag |= flag : this->flag &= ~flag;
+}
+
+uint8_t Computer::Cpu::get_flag(void) {
+  return this->flag;
+}
 
 void Computer::Cpu::attach_to_bus(Bus *bus) {
   this->bus = bus;
@@ -14,4 +23,34 @@ void Computer::Cpu::write(uint8_t address, uint16_t value) {
 
 uint16_t Computer::Cpu::read(uint8_t address) {
   return this->bus->read(address);
+}
+
+void Computer::Cpu::clock(void) {
+  if (this->clock_cycles == 0x00) {
+    this->opcode = this->read(this->program_counter);
+    this->program_counter++;
+
+    this->clock_cycles = this->instructions[this->opcode].clock_cycles;
+
+    uint8_t address_mode_cycles = this->instructions[this->opcode].address_mode();
+    uint8_t execution_cycles = this->instructions[this->opcode].execute();
+
+    this->clock_cycles += (address_mode_cycles + execution_cycles);
+  }
+
+  this->clock_cycles--;
+}
+
+void Computer::Cpu::reset(void) {
+  this->clock_cycles = 0x00;
+  this->opcode = 0x00;
+  this->flag = 0x00;
+  this->accumulator = 0x00;
+  this->register_x = 0x00;
+  this->register_y = 0x00;
+  this->stack_pointer = 0x00;
+  this->program_counter = 0x00;
+
+  this->instruction_entry_point_address = 0x00;
+  this->instruction_relative_point_address = 0x00;
 }
--- src/Computer/Cpu.h
+++ src/Computer/Cpu.h
@@ -1,20 +1,72 @@
 #pragma once
 
+#include <array>
+#include <string>
 #include "Bus.h"
+
+enum FLAGS {
+  Carry = (1 << 0),
+  Zero = (1 << 1),
+  InterruptDisable = (1 << 2),
+  Decimal = (1 << 3),
+  Break = (1 << 4),
+  BFlag = (1 << 5),
+  Overflow = (1 << 6),
+  Negative = (1 << 7),
+};
+
+typedef struct Instrucion {
+  std::string name;
+  uint8_t clock_cycles = 0x00;
+
+  uint8_t(*execute)(void) = nullptr; // execute a opcode function
+  uint8_t(*address_mode)(void) = nullptr;
+} Instrucion;
 
 namespace Computer {
 
 class Cpu {
   private:
     Bus *bus = nullptr;
+    uint8_t clock_cycles = 0x00;
+    uint8_t opcode = 0x00;
+
+    uint16_t instruction_entry_point_address = 0x00;
+    uint16_t instruction_relative_point_address = 0x00;
+
+    // registers
+    uint8_t flag = 0x00;
+    uint8_t accumulator = 0x00;
+    uint8_t register_x = 0x00;
+    uint8_t register_y = 0x00;
+    uint8_t stack_pointer = 0x00;
+    uint8_t program_counter = 0x00;
+
+    void set_flag(FLAGS flag, bool value);
+    uint8_t get_flag(void);
    
   public:
-    Cpu();
-    ~Cpu();
+    Cpu(void);
+    ~Cpu(void);
 
     void attach_to_bus(Bus *bus);
+
     void write(uint8_t address, uint16_t value);
     uint16_t read(uint8_t address);
+
+    // external signals
+    void clock(void);
+    void reset(void);
+    void interrupt_request(void);
+    void non_maskable(void);
+
+    std::array<Instrucion, 56> instructions;
+
+    // opcodes (56)
+    // impl later
+
+    // addressing modes (12)
+    // impl later
 };
 
 }
